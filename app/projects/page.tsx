51195b8fa228e315921341ad999a96bd78077474
liumu96 <918883717@qq.com>
2023-09-29 16:48:08 +0800

    code submit

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -5,6 +5,9 @@ const Projects = () => {
   return (
     <div className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 my-10 py-8 sm:py-16 md:py-20">
       <SectionHeading>Projects</SectionHeading>
+      <span className="font-bold text-4xl bg-gradient-to-r from-pink-300 to-purple-400 text-transparent bg-clip-text px-6 py-3 rounded-md">
+        TODO
+      </span>
     </div>
   );
 };
